0d56c76acaba3ac79c9fab63ddafa2ab3d9038f6
Vítor Galvão <info@vitorgalvao.com>
2016-05-29 16:24:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# <img src='https://i.imgur.com/xzPZ3rf.png' width='45' align='left' alt='appear.in logo'>appear.in (unofficial app)
+
+Use [appear.in](https://appear.in/) just like you do in your browser, but as a desktop app. This app will also notify you in its icon when messages are received.
+
+<img src='https://i.imgur.com/OcSOiEi.jpg' alt='appear.in screenshot'>
+
+## Install
+
+[Download the lastest version](https://github.com/vitorgalvao/appear.in/releases).
+
+## Development
+
+Built with [Electron](http://electron.atom.io).
+
+##### Commands
+
+- Install dependencies: `npm install`
+- Run: `npm start`
+- Build for OS X: `npm run build-osx`
+- Build for all platforms: `npm run build`
+
+Currently, only OS X is supported. I do not intend to officially support other platforms in the near future since I cannot consistently and reliably test on them, but am willing to add support if someone wants to collaborate in doing the legwork.
+
+#### License
+
+The Unlicense (Public Domain, essentially)
+
+---
+
+#### Support the authors
+
+To support [appear.in](https://appear.in/), authors of the website, check the information on their upcoming premium offerings.
+
+[<img src='https://dl.dropboxusercontent.com/s/suezj117j0cggq0/appear.in.svg' height='28' alt='appear.in premium'>](https://appear.in/information/premium/)
+
+To support [Github](https://github.com/), authors of Electron, subscribe to a plan.
+
+[<img src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' height='20' alt='Subscribe to Githb plan'>](https://github.com/pricing)
+
+To support the author of this app, pick a method below.
+
+[<img src='https://upload.wikimedia.org/wikipedia/commons/5/53/PayPal_2014_logo.svg' height='18' alt='Support via Paypal'>](https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=hgdesigns%40gmail%2ecom&item_name=Tip%20for%20vitorgalvao&currency_code=EUR)&nbsp;&nbsp;
+[<img src='https://upload.wikimedia.org/wikipedia/commons/c/c5/Bitcoin_logo.svg' height='15' alt='Support via Bitcoin'>](https://dl.dropboxusercontent.com/s/n8k06vxyver48yf/bitcoin_tip_jar.html)
---
+++ app/main.js
@@ -0,0 +1,99 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const ipcMain = electron.ipcMain;
+
+let mainWindow;
+
+// Main app menu
+const Menu = electron.Menu;
+const name = electron.app.getName();
+const webviewId = 'appearin_webview';
+const appWebsite = 'https://github.com/vitorgalvao/appear.in/';
+const template = [
+  {
+    label: 'Application',
+    submenu: [
+      { label: 'About ' + name, role: 'about' },
+      { type: 'separator' },
+      { label: 'Hide ' + name, accelerator: 'Command+H', role: 'hide' },
+      { label: 'Hide Others', accelerator: 'Command+Shift+H', role: 'hideothers' },
+      { type: 'separator' },
+      { label: 'Quit', accelerator: 'Command+Q', click: function() {app.quit();} }
+    ]
+  }, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'Command+Z', role: 'undo' },
+      { label: 'Redo', accelerator: 'Shift+Command+Z', role: 'redo' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'Command+X', role: 'cut' },
+      { label: 'Copy', accelerator: 'Command+C', role: 'copy' },
+      { label: 'Paste', accelerator: 'Command+V', role: 'paste' },
+      { label: 'Select All', accelerator: 'Command+A', role: 'selectall' }
+    ]
+  }, {
+    label: 'View',
+    submenu: [
+      { label: 'Reload', accelerator: 'Command+R', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.webContents.executeJavaScript('document.getElementById(' + webviewId + ').reload()');} }
+    ]
+  }, {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      { label: 'Minimize', accelerator: 'Command+M', role: 'minimize' },
+      { label: 'Close', accelerator: 'Command+W', role: 'close' }
+    ]
+  }, {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      { label: 'View Website', click: function() { require('electron').shell.openExternal(appWebsite) } },
+      { type: 'separator' },
+      { label: 'Toggle Developer Tools', click: function(item,focusedWindow) {if (focusedWindow) focusedWindow.toggleDevTools();} }
+    ]
+  }
+];
+
+function focus_webview() {
+  mainWindow.webContents.executeJavaScript('document.getElementById("appearin_webview").focus();');
+}
+
+// Setting app notifications.
+ipcMain.on('unread', function(event, args) {
+  let unreadMessages;
+
+  if (args === '0') {
+    unreadMessages = '•';
+  } else {
+    unreadMessages = args;
+  }
+
+  if (! mainWindow.isFocused()) {
+    app.dock.bounce();
+    app.dock.setBadge(unreadMessages);
+  }
+});
+
+app.on('browser-window-focus', function() {
+  app.dock.setBadge('');
+  focus_webview();
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 1000, height: 596, titleBarStyle: 'hidden-inset'});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  focus_webview();
+
+  // Set menu
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+  // Prevent app from exiting (hide it instead) when window is closed (i.e. when we press the red close button).
+  mainWindow.on('close', function(event) {
+    event.preventDefault();
+    mainWindow.hide();
+    app.focus();
+  });
+  app.on('before-quit', function() { app.exit(0); });
+  app.on('activate', function() { mainWindow.show(); });
+});
